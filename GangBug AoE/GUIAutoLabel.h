54b4c1ec625dcb36bab0df8b582412a29f9398ab
CapitanLiteral <anaraguen@gmail.com>
2017-03-21 22:24:03 +0100

    AutoLabel creation
    Pretends to watch and display a var value

---
+++ GangBug AoE/GUIAutoLabel.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "GUILabel.h"
+#include "GB_Rectangle.h"
+#include <string>
+
+template <class t>
+class GUIAutoLabel :
+	public GUILabel
+{
+public:
+	GUIAutoLabel(GB_Rectangle<int> position, t * _watcher)
+	{
+		watchingVar = _watcher;
+		SetRectangle(position);
+	}
+	~GUIAutoLabel()
+	{
+	}
+	void Update(const GUIElement * mouseHover, const GUIElement * focus)
+	{
+		std::string str = std::to_string(*watchingVar);
+		SetText(str.c_str());
+	}
+
+	t* watchingVar;
+
+
+};
+
