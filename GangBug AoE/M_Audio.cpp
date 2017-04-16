9f27236be985f60ab5955adad7145d3622079ff9
SrPerso <carlos@jardiper.es>
2017-04-16 19:46:30 +0200

    Audio engine Changed

--- GangBug AoE/M_Audio.cpp
+++ GangBug AoE/M_Audio.cpp
@@ -3,97 +3,22 @@
 #include "M_FileSystem.h"
 #include "M_Audio.h"
 
-
+#include "SDL/include/SDL.h"
+#include "SDL_mixer\include\SDL_mixer.h"
 #pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib" )
 
-//#include "SDL/include/SDL.h"
-//#include "SDL_mixer\include\SDL_mixer.h"
-
-void AudioFX::Play(int loops/*==0*/)
-{//loop -1 == loop 4ever, 0== play one time, 1== play two times...
-
-	if (Mix_PlayChannel(-1, mChunk, loops) == -1) {
-		LOG("Mix_PlayChanel! SDL_Error: %s\n", SDL_GetError());
-	}
-}
-bool AudioMusic::Play(int loops/*==-1*/, float fade_time)
-{//loop -1 == loop 4ever, 0== play 0 times, 1== play 1 times, 2== play 2 times...
-
-	bool ret = false;
-	if (mMusic != NULL)
-	{
-		if (fade_time > 0.0f)
-		{
-			Mix_FadeOutMusic((int)(fade_time * 1000.0f));
-		}
-		else
-		{
-			Mix_HaltMusic();
-		}
-
-		// this call blocks until fade out is done
-		Mix_FreeMusic(mMusic);
-	}
-
-	mMusic = Mix_LoadMUS(this->path);
-
-	if (mMusic == NULL)
-	{
-		LOG("Cannot load music %s. Mix_GetError(): %s\n", this->path, Mix_GetError());
-		ret = false;
-	}
-
-	else
-	{
-		if (fade_time > 0.0f)
-		{
-			if (Mix_FadeInMusic(mMusic, loops, (int)(fade_time * 1000.0f)) < 0)
-			{
-				LOG("Cannot fade in music %s. Mix_GetError(): %s", path, Mix_GetError());
-				ret = false;
-			}
-		}
-		else
-		{
-			if (Mix_PlayMusic(mMusic, loops) < 0)
-			{
-				LOG("Cannot play in music %s. Mix_GetError(): %s", path, Mix_GetError());
-				ret = false;
-			}
-		}
-	}
-
-
-	LOG("Successfully playing %s", path);
-	return ret;
-}
-
-void AudioMusic::Pause()
+M_Audio::M_Audio(bool startEnabled) : Module(startEnabled)
 {
-	Mix_PauseMusic();
+	music = NULL;
+	name.assign("audio");
 }
 
-void AudioMusic::Stop()
-{
-	Mix_HaltMusic();
-}
-
-void AudioMusic::Resume()
-{
-	Mix_ResumeMusic();
-}
-
-//------------------------------------------ Audio engine
-M_Audio::M_Audio(bool start_enabled) : Module(start_enabled)
-{
-}
-
+// Destructor
 M_Audio::~M_Audio()
-{
-}
+{}
 
-//initialice audio
-bool M_Audio::Awake(pugi::xml_node &)
+// Called before render is available
+bool M_Audio::Awake(pugi::xml_node& config)
 {
 	LOG("Loading Audio Mixer");
 	bool ret = true;
@@ -102,106 +27,199 @@ bool M_Audio::Awake(pugi::xml_node &)
 	if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0)
 	{
 		LOG("SDL_INIT_AUDIO could not initialize! SDL_Error: %s\n", SDL_GetError());
-		ret = false;
+		active = false;
+		ret = true;
 	}
 
-	// load support for the OGG format
-	int flags = MIX_INIT_OGG;	// can support MIX_INIT_FAC, MIX_INIT_MP3, MIX_INIT_OGG, MIX_INIT_MOD,
+	// load support for the JPG and PNG image formats
+	int flags = MIX_INIT_OGG;
 	int init = Mix_Init(flags);
 
 	if ((init & flags) != flags)
 	{
 		LOG("Could not initialize Mixer lib. Mix_Init: %s", Mix_GetError());
-		ret = false;
+		active = false;
+		ret = true;
 	}
 
 	//Initialize SDL_mixer
-	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
 	{
 		LOG("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+		active = false;
 		ret = true;
-		mIsInitialized = true;
+	}
+
+	menuMusic = Mix_LoadMUS_RW(app->fs->Load("audio/BSO/BSO_Menu.ogg"), 1);
+	thirdMission = Mix_LoadMUS_RW(app->fs->Load("audio/BSO/BSO_ThirdMision.ogg"), 1);
+
+	return ret;
+}
+
+// Called before quitting
+bool M_Audio::CleanUp()
+{
+	if (!active)
+		return true;
+
+	LOG("Freeing sound FX, closing Mixer and Audio subsystem");
+
+	if (music != NULL)
+	{
+		//Mix_FreeMusic(music); //TODO
+	}
+
+	if (thirdMission != NULL)
+	{
+		//Mix_FreeMusic(thirdMission); //TODO
+	}
+
+	if (menuMusic != NULL)
+	{
+		//Mix_FreeMusic(menuMusic); //TODO
+	}
+
+	std::list<Mix_Chunk*>::iterator item;
+	for (item = fx.begin(); item != fx.end(); ++item)
+		Mix_FreeChunk(*item);
+
+	fx.clear();
+
+	Mix_CloseAudio();
+	Mix_Quit();
+	SDL_QuitSubSystem(SDL_INIT_AUDIO);
+
+	return true;
+}
+
+bool M_Audio::CleanData()
+{
+	if (!active)
+		return true;
+
+	LOG("Freeing sound FX and music");
+
+	if (music != NULL)
+	{
+		Mix_FreeMusic(music);
+	}
+
+	std::list<Mix_Chunk*>::iterator item;
+	for (item = fx.begin(); item != fx.end(); ++item)
+	{
+		Mix_FreeChunk(*item);
+	}
+
+
+	fx.clear();
+	Mix_HaltMusic();
+
+	return true;
+}
+
+// Play a music file
+bool M_Audio::PlayMusic(const char* path, float fade_time)
+{
+	bool ret = true;
+
+	if (!active)
+		return false;
+
+	if (music != nullptr)
+	{
+		if (fade_time > 0.0f)
+		{
+			Mix_FadeOutMusic(int(fade_time * 1000.0f));
+		}
+		else
+		{
+			Mix_HaltMusic();
+		}
+
+		// this call blocks until fade out is done
+		Mix_FreeMusic(music);
+	}
+
+	music = Mix_LoadMUS_RW(app->fs->Load(path), 1);
+
+	if (music == NULL)
+	{
+		LOG("Cannot load music %s. Mix_GetError(): %s\n", path, Mix_GetError());
+		ret = false;
+	}
+	else
+	{
+		if (fade_time > 0.0f)
+		{
+			if (Mix_FadeInMusic(music, -1, (int)(fade_time * 1000.0f)) < 0)
+			{
+				LOG("Cannot fade in music %s. Mix_GetError(): %s", path, Mix_GetError());
+				ret = false;
+			}
+		}
+		else
+		{
+			if (Mix_PlayMusic(music, -1) < 0)
+			{
+				LOG("Cannot play in music %s. Mix_GetError(): %s", path, Mix_GetError());
+				ret = false;
+			}
+		}
+	}
+
+	LOG("Successfully playing %s", path);
+	return ret;
+}
+
+bool M_Audio::PlayTheme(_Mix_Music* theme)
+{
+	bool ret = true;
+	if (Mix_PlayMusic(theme, -1) < 0)
+	{
+		LOG("Cannot play music Mix_GetError(): %s", Mix_GetError());
+		ret = false;
 	}
 
 	return ret;
 }
 
-
-
-//destroy all the elements
-bool M_Audio::CleanUp()
+// Load WAV
+unsigned int M_Audio::LoadFx(const char* path)
 {
-	if (mIsInitialized) {
-		mIsInitialized = false;
-		Mix_CloseAudio();
-		Mix_Quit();
-		SDL_QuitSubSystem(SDL_INIT_AUDIO);
+	unsigned int ret = 0;
+
+	if (!active)
+		return 0;
+
+	Mix_Chunk* chunk = Mix_LoadWAV_RW(app->fs->Load(path), 1);
+
+	if (chunk == NULL)
+	{
+		LOG("Cannot load wav %s. Mix_GetError(): %s", path, Mix_GetError());
+	}
+	else
+	{
+		fx.push_back(chunk);
+		ret = fx.size();
 	}
 
-	return true;
-
+	return ret;
 }
 
-void M_Audio::DrawDebug()
+// Play WAV
+bool M_Audio::PlayFx(unsigned int id, int repeat)
 {
+	bool ret = false;
+
+	if (!active)
+		return false;
+
+	if (id > 0 && id <= fx.size())
+	{
+		std::list<Mix_Chunk*>::iterator item = fx.begin();
+		advance(item, id - 1);
+		Mix_PlayChannel(-1, (*item), repeat);
+	}
+
+	return ret;
 }
-
-AudioFX* M_Audio::LoadAudioFX(const std::string & filePath)
-{
-	//look to the cachhe to found the audio
-	auto it = mEffectMap.find(filePath);
-
-	AudioFX ret;
-
-	if (it == mEffectMap.end()) {
-		//failed to find -> load
-		Mix_Chunk* chunk = Mix_LoadWAV(filePath.c_str());
-
-		if (chunk == nullptr) {
-			LOG("Mix_LoadWAV! SDL_Error: %s\n", SDL_GetError());
-		}
-
-		ret.mChunk = chunk;
-		mEffectMap[filePath] == chunk;
-		ret.path = filePath.c_str();
-
-	}
-	else {
-		//is on cache
-		ret.mChunk = it->second;
-		ret.path = filePath.c_str();
-	}
-
-
-	return &ret;
-}
-
-AudioMusic* M_Audio::LoadAudioMusic(const std::string & filePath)
-{
-
-	//look to the cachhe to found the audio
-	auto it = mMusicMap.find(filePath);
-
-	AudioMusic ret;
-
-	if (it == mMusicMap.end()) {
-		//failed to find -> load
-		Mix_Music* MixMusic = Mix_LoadMUS(filePath.c_str());
-
-		if (MixMusic == nullptr) {
-			LOG("Mix_LoadWAV! SDL_Error: %s\n", SDL_GetError());
-		}
-
-		ret.mMusic = MixMusic;
-		mMusicMap[filePath] == MixMusic;
-		ret.path = filePath.c_str();
-	}
-	else {
-		//is on cache
-		ret.mMusic = it->second;
-		ret.path = filePath.c_str();
-	}
-
-	return &ret;
-}
-//------------------------------------------
--- GangBug AoE/M_Audio.h
+++ GangBug AoE/M_Audio.h
@@ -1,114 +1,51 @@
-#ifndef __M_AUDIO_H__
-#define __M_AUDIO_H__
-#include "Module.h"
-#include "SDL_mixer\include\SDL_mixer.h"
-#include <map> 
 
+#ifndef __j1AUDIO_H__
+#define __j1AUDIO_H__
+
+#include "Module.h"
+#include <list>
 
 #define DEFAULT_MUSIC_FADE_TIME 2.0f
 
-//------------------------
-class AudioFX {
-	friend class M_Audio;
-public:
-	void Play(int nLoops = 0);//default will play once
+struct _Mix_Music;
+struct Mix_Chunk;
 
-private:
-	Mix_Chunk* mChunk = nullptr;
-	const char* path;
-};
-//------------------------
-
-//------------------------
-class AudioMusic {
-	friend class M_Audio;
-public:
-	bool Play(int nLoops = -1, float fade_time = DEFAULT_MUSIC_FADE_TIME);//default will play once
-	static void Pause();
-	static void Stop();
-	static void Resume();
-
-
-private:
-	Mix_Music* mMusic = nullptr;
-	const char* path;
-};
-//------------------------
-
-//------------------------
-class M_Audio : public Module {
+class M_Audio : public Module
+{
 public:
 
-	M_Audio(bool startEnabled = true);
+	M_Audio(bool);
 
 	// Destructor
 	virtual ~M_Audio();
 
 	// Called before render is available
-	bool Awake(pugi::xml_node&)override;
-	
+	bool Awake(pugi::xml_node&);
+
 	// Called before quitting
-	bool CleanUp()override;
+	bool CleanUp();
 
-	void DrawDebug()override;
+	//Called between scenes
+	bool CleanData();
 
+	// Play a music file
+	bool PlayMusic(const char* path, float fade_time = DEFAULT_MUSIC_FADE_TIME);
 
-public:
-	AudioFX* LoadAudioFX(const std::string&filePath);
-	AudioMusic* LoadAudioMusic(const std::string&filePath);
+	bool PlayTheme(_Mix_Music* theme);
 
+	// Load a WAV in memory
+	unsigned int LoadFx(const char* path);
+
+	// Play a previously loaded WAV
+	bool PlayFx(unsigned int fx, int repeat = 0);
+
+	_Mix_Music*				menuMusic = NULL;
+	_Mix_Music*				thirdMission = nullptr;
 private:
 
-	std::map<std::string, Mix_Chunk*> mEffectMap;
-	std::map<std::string, Mix_Music*> mMusicMap;
+	_Mix_Music*				music = NULL;
+	std::list<Mix_Chunk*>	fx;
 
-	bool mIsInitialized = false;
 };
-//------------------------
 
-//how to implemement:
-
-/*
-
-///in Application.h
-
-#include "ModuleAudioEngine.h"
-ModuleAudioEngine* audio;
-
-///in Application.cpp
-
-audio = new ModuleAudioEngine(this, true);
-AddModule(audio); <- Add before Scene module;
-
-
-///in module scene.h   !!!!!!!!!!!!!!!!!!!!!!!!!!!!! well in this case no, i will work with it !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-AudioMusic* bso_scene;
-
-///in module scene.cpp
-
-(in the start();)
-App->audio->Init();
-bso_scene = App->audio->LoadAudioMusic("Sound/BSO/BSO_SCENE1.ogg");
-
-(then to the efects u can implement it on the monster class or in sscene too)
-
-///.h
-AudioFX grr_fx;
-
-///.cpp
-
-(constructor (or start in module scene))
-grr_fx = App->audio->LoadAudioFX("Sound/FX/grr_fx.wav");
-
-
-
-///// To use:
-grr_fx.play(); default 0
-AudioFX.play(); default -1
-
-
-//dont forget to Clean it!
-*/
-
-#endif // __M_AUDIO_H__
+#endif // __j1AUDIO_H__
